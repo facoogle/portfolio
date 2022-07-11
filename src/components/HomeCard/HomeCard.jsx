d9e97d8a6dd981aad138d535466fab92d640906b
facoogle <facoogle@gmail.com>
2022-07-10 21:41:27 -0300

    actualizacion 2

--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -6,19 +6,19 @@ export default function HomeCard(){
         <div className="HomeCard">
         <div class="card mb-3" >
   <div class="row g-0">
-    <div class="col-md-4">
+    <div class="col">
       <img class="img1" src="./yo2.png"   alt="..."/>
     </div>
     <div class="col-md-4">
-    <div class="contenedor">I'm<ul className="move">
+    <div class="contenedor">Soy<ul className="move">
             <li>Facundo</li>
-            <li>Full Stack Developer</li>
-            <li>negro</li>
+            <li>Full Stack</li>
+            <li>Developer</li>
+            
         </ul></div>
       <div class="card-body">
         <h5 class="card-title">Hola, Bievenidos a mi portafolio..! <span>&#160;</span></h5>
-    
-        
+
       </div>
     </div>
   </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,26 +2,27 @@ import "./Skills.css"
 
 function Skills() {
     return (
+       <div className="skill">
+        <h1>Skills</h1>
         <div className="skills">
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"/><p> REACT</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" /><p> REDUX</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" /><p> HTML</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" /><p>CSS3</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" /><p>JAVASCRIPT</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" /><p>EXPRESS</p></div>
                 
-                <div className="imgsdiv">
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg" />
-                <img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg" />
-                </div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" /><p>NPM</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" /><p>POSTGRES</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" /><p>GITHUB</p></div>
+                
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" /><p>TYPESCRIPT</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" /><p>NODE</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" /><p>BOOSTRAP</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" /><p>GIT</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg" /><p>HEROKU</p></div>
+                <div className="text"><img className="imgs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg" /><p>SEQUELIZE</p></div>
+        </div>
         </div>
     )
 }
